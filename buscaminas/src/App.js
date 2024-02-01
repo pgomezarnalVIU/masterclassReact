6b0a0a4ca3bd4086d4725b69ac3f3ee34e3bdee9
Paco Mastermind <paco@mastermind.ac>
2024-02-01 23:20:00 +0100

    HTML to JSX

--- buscaminas/src/App.js
+++ buscaminas/src/App.js
@@ -2,9 +2,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+  <div className="container text-center" style={{ width: 340 }}>
+    {/*TABLERO*/}
+    <div className="grid bg-body-secondary py-2 px-4 borderOutSide m-0">
+      {/*MARCADOR*/}
+      <div className="row bg-body-secondary borderInside ">
+        <div className="d-flex flex-wrap justify-content-around">
+          <div className="lcdText text-danger pe-2 m-2 borderInsideS">10</div>
+          <div className="align-self-center m-2 borderInsideS">
+            <img src="acierto.png" style={{ width: 50 }} />
+          </div>
+          <div
+            className="lcdText text-danger pe-2 m-2 borderInsideS"
+            style={{ width: 54 }}
+          >
+            00
+          </div>
+        </div>
+      </div>
+      {/*FIN MARCADOR*/}
+      {/*CELDAS*/}
+      <div className="row borderInside bg-body-secondary text-center justify-content-center">
+        <div className="col my-1 p-0"></div>
+      </div>
+      {/*FIN CELDAS*/}
     </div>
+    {/*FIN TABLERO*/}
+  </div>
   );
 }
 
