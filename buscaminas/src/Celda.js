5a06024f79f8bbfa5a763d1f2b0a0a2a6d4e178e
Paco Mastermind <paco@mastermind.ac>
2024-02-04 19:46:48 +0100

    Renderizado condicional

--- buscaminas/src/Celda.js
+++ buscaminas/src/Celda.js
@@ -2,7 +2,7 @@ function Celda({ valor=""}) {
     return (
         <>
             <button className="border border-2 border-dark-subtle fs-2 fw-bold text-success" style={{minWidth:50,minHeight:50}}>
-                {valor}
+                {valor==" "?"\u00A0":valor}
             </button>
         </>
     )
